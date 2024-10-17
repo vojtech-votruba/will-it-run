a5d7b77e42e299c209f7aff3135617cfc2402ce0
adalbert353 <vojtech.votruba@seznam.cz>
2024-10-17 22:25:50 +0200

    finished the scraper

--- will-it-run/scraper.py
+++ will-it-run/scraper.py
@@ -1,13 +1,22 @@
 import requests
 import platform
+from difflib import SequenceMatcher
 
 def search(name: str):
     name = name.lower()
 
     URL_games = "https://api.steampowered.com/ISteamApps/GetAppList/v2/"
     raw_data = requests.get(URL_games).json()["applist"]["apps"]
+    
+    # Basic linear search because I was to lazy to implement binary, lol
+    app_id = ""
+    max_alignment = 0
 
-    app_id = "17450"
+    for app in raw_data:
+        alignment = SequenceMatcher(None, name, app["name"].lower()).ratio() 
+        if alignment > max_alignment:
+            max_alignment = alignment
+            app_id = str(app["appid"])
 
     URL_requirements = "https://store.steampowered.com/api/appdetails?appids="
     res = requests.get(URL_requirements + app_id).json()
